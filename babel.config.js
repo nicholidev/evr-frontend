9f372796a06bcc7ff532045c16efdbc2367f5be4
nicholidev <nicholijin@outlook.com>
2022-11-22 01:31:50 +0800

    (feat)/fix build issue

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,16 @@
-module.exports = function (api) {
+module.exports = function (api) 
+{
     api.cache(true);
 
     const presets = ["next/babel"];
     const plugins = [
-        ["import", {libraryName: "antd", style: true}],
+        ["import", { libraryName: "antd", style: true }],
         [
             "styled-components",
             {
                 ssr: true,
             },
-        ],
-        ["babel-plugin-module-resolver", {
-            "alias": {
-              "^effector$": "effector/compat",
-              "^effector-react$": "effector-react/compat"
-            }
-        }]
+        ]
     ];
 
     return {
